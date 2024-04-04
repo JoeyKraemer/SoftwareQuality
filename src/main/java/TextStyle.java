e743f8abe2f5e36f69d23566b8b9ec595a75daa1
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2024-04-04 11:18:57 +0200

    ADD: third style (text style)

---
+++ src/main/java/TextStyle.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class TextStyle extends Style{
+    public TextStyle(int indent, Color color, int fontSize, int leading) {
+        super(indent, color, fontSize, leading);
+    }
+}
