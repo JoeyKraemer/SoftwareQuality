8f511d49164be36c96cbd480f60f1ecca0a99638
Joey Krämer <joey.kramer@student.nhlstenden.com>
2024-04-07 10:53:23 +0200

    delete unused variable

--- src/main/java/jabberpoint/JabberPoint.java
+++ src/main/java/jabberpoint/JabberPoint.java
@@ -27,7 +27,6 @@ public class JabberPoint {
 	/** The Main Program */
 	public static void main(String argv[]) {
 		
-		//jabberpoint.style.builder.StyleBuilder.createStyle();
 		Presentation presentation = new Presentation();
 		new SlideViewerFrame(JABVERSION, presentation);
 		try {
