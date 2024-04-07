91e5f43aaadcbedf4e922855b4b9844f81d20a4f
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2024-04-07 12:57:32 +0200

    fixed setters in SubtitleOneStyleBuilder

--- src/main/java/jabberpoint/style/builder/SubtitleOneStyleBuilder.java
+++ src/main/java/jabberpoint/style/builder/SubtitleOneStyleBuilder.java
@@ -12,22 +12,22 @@ public class SubtitleOneStyleBuilder implements StyleBuilder{
     private int leading;
     @Override
     public void setIndent(int indent) {
-
+        this.indent = indent;
     }
 
     @Override
     public void setColor(Color color) {
-
+        this.color = color;
     }
 
     @Override
     public void setFontSize(int fontSize) {
-
+        this.fontSize = fontSize;
     }
 
     @Override
     public void setLeading(int leading) {
-
+        this.leading = leading;
     }
 
     public SubtitleOneStyle getResult() {
