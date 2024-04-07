a9c4726daaa55bf531561de4175b41769e78da92
Joey Krämer <joey.kramer@student.nhlstenden.com>
2024-04-07 21:30:34 +0200

    refactor accessor files

--- src/main/java/jabberpoint/accessor/Accessor.java
+++ src/main/java/jabberpoint/accessor/Accessor.java
@@ -15,18 +15,17 @@ import java.io.IOException;
  */
 
 public abstract class Accessor {
-    public static final String DEMO_NAME = "Demonstration presentation";
-    public static final String DEFAULT_EXTENSION = ".xml";
+
+    public Accessor() {
+
+    }
 
     public static Accessor getDemoAccessor() {
         return new DemoPresentation();
     }
 
-    public Accessor() {
-    }
+    public abstract  void loadFile(Presentation p, String fn) throws IOException;
 
-    abstract public void loadFile(Presentation p, String fn) throws IOException;
-
-    abstract public void saveFile(Presentation p, String fn) throws IOException;
+    public abstract  void saveFile(Presentation p, String fn) throws IOException;
 
 }
