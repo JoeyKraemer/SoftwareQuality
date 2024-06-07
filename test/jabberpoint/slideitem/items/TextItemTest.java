2428b7d6e31b70ee3681376c41197a260c8d0a13
Joey Krämr <>
2024-06-07 09:53:35 +0200

    improve naming of test methods

--- test/jabberpoint/slideitem/items/TextItemTest.java
+++ test/jabberpoint/slideitem/items/TextItemTest.java
@@ -16,34 +16,34 @@ class TextItemTest {
     private TextItem textItem;
 
     @BeforeEach
-    void setUp() {
+    void setup() {
         textItem = new TextItem();
     }
 
     @Test
-    void testCreateSlideItem() {
+    void createSlideItem_checkFileTypeAndText_instanceOfTextItem() {
         SlideItem slideItem = textItem.createSlideItem();
 
         assertNotNull(slideItem, "SlideItem should not be null");
-        assertTrue(slideItem instanceof TextItem, "SlideItem should be an instance of TextItem");
-        assertEquals("text", ((TextItem) slideItem).getText(), "The text should be 'text'");
+        assertInstanceOf(TextItem.class, slideItem, "SlideItem should be an instance of TextItem");
+        assertEquals("text", slideItem.getText(), "The text should be 'text'");
     }
 
     @Test
-    void testCreateSlideItemWithStyleAndText() {
+    void createSlideItemWithStyleAndText_checkFileTypeAndContent_instanceOfTextItemAndContentMatches() {
         Style mockStyle = mock(Style.class);
         String testText = "Test Text";
 
         SlideItem slideItem = textItem.createSlideItem(mockStyle, testText);
 
         assertNotNull(slideItem, "SlideItem should not be null");
-        assertTrue(slideItem instanceof TextItem, "SlideItem should be an instance of TextItem");
-        assertEquals(testText, ((TextItem) slideItem).getText(), "The text should match the one provided");
-        assertEquals(mockStyle, ((TextItem) slideItem).getStyle(), "The style should match the one provided");
+        assertInstanceOf(TextItem.class, slideItem, "SlideItem should be an instance of TextItem");
+        assertEquals(testText, slideItem.getText(), "The text should match the one provided");
+        assertEquals(mockStyle, slideItem.getStyle(), "The style should match the one provided");
     }
 
     @Test
-    void testCreateSlideItemWithEmptyTextThrowsException() {
+    void createSlideItemWithEmptyTextThrowsException_createWithoutText_exceptionThrown() {
         Style mockStyle = mock(Style.class);
 
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
@@ -54,7 +54,7 @@ class TextItemTest {
     }
 
     @Test
-    void testCreateSlideItemWithNullStyleThrowsException() {
+    void createSlideItemWithEmptyTextThrowsException_createWithoutStyle_exceptionThrown() {
         String testText = "Test Text";
 
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
@@ -62,44 +62,5 @@ class TextItemTest {
         });
 
         assertEquals("Style cant be null", thrown.getMessage(), "Exception message should match expected message");
-    }
-
-    @Test
-    void testGetBoundingBox() {
-        Graphics mockGraphics = mock(Graphics.class);
-        ImageObserver mockObserver = mock(ImageObserver.class);
-        Style mockStyle = mock(Style.class);
-
-        when(mockStyle.getIndent()).thenReturn(10);
-        when(mockStyle.getLeading()).thenReturn(20);
-        when(mockStyle.getColor()).thenReturn(Color.BLACK);
-
-        textItem.createSlideItem(mockStyle, "Test Text");
-
-        Rectangle boundingBox = textItem.getBoundingBox(mockGraphics, mockObserver, 1.0f);
-
-        assertNotNull(boundingBox, "BoundingBox should not be null");
-        assertTrue(boundingBox.width > 0, "BoundingBox width should be greater than 0");
-        assertTrue(boundingBox.height > 0, "BoundingBox height should be greater than 0");
-    }
-
-    @Test
-    void testDraw() {
-        Graphics mockGraphics = mock(Graphics.class);
-        ImageObserver mockObserver = mock(ImageObserver.class);
-        Style mockStyle = mock(Style.class);
-
-        when(mockStyle.getIndent()).thenReturn(10);
-        when(mockStyle.getLeading()).thenReturn(20);
-        when(mockStyle.getColor()).thenReturn(Color.BLACK);
-
-        textItem.createSlideItem(mockStyle, "Test Text");
-
-        BufferedImage bufferedImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = bufferedImage.createGraphics();
-
-        textItem.draw(0, 0, 1.0f, g2d, mockObserver);
-
-        verify(mockGraphics, never()).drawString(anyString(), anyInt(), anyInt());
     }
 }
